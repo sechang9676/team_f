aec52fe65993674830b11cc9d573a10102515f18
vittroi384 <eunsol924@naver.com>
2021-09-16 00:55:30 +0900

    hello.java 파일

--- Game/src/ex01/Hello.java
+++ Game/src/ex01/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello World!");
+		System.out.println("203629_ÀüÇĞ¼ö")
 	}
 
 }
